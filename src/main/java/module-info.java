48ecd6000fba0c88c23d63275af2eb14da4656ec
Firas Chabchoub <firaschabchoub@hotmail.com>
2024-12-11 18:13:47 +0100

    Creation des services et entitées

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module org.example.teleporti {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
 
     opens org.example.teleporti to javafx.fxml;
     exports org.example.teleporti;
