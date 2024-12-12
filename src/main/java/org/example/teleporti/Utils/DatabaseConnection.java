3a13d5d00bb46a0dd8fd20ecbb216b70cacc6121
Firas Chabchoub <firaschabchoub@hotmail.com>
2024-12-12 01:04:44 +0100

    Creation des services et entitées

--- src/main/java/org/example/teleporti/Utils/DatabaseConnection.java
+++ src/main/java/org/example/teleporti/Utils/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private Connection con;
-    private String dburl = "jdbc:mysql://localhost:3306/covoiturage";
+    private String dburl = "jdbc:mysql://localhost:3306/teleporti";
     private String dbuser = "root";
     private String dbpassword = "";
 
