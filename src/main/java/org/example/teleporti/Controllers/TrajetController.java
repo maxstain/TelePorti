c5435df4b1a8e0cfa01a877ba4866ebea45f4eda
Ahmedbha <ahmedbha25@gmail.com>
2024-12-17 20:09:48 +0100

    ajouter le service trajet

---
+++ src/main/java/org/example/teleporti/Controllers/TrajetController.java
@@ -0,0 +1,36 @@
+package org.example.teleporti.Controllers;
+
+import org.example.teleporti.Entities.Trajet;
+import org.example.teleporti.Services.ServiceTrajet;
+import org.example.teleporti.Utils.DatabaseConnection;
+
+import java.sql.Connection;
+
+public class TrajetController {
+    private static Connection con = new DatabaseConnection().getConnection();
+    private static ServiceTrajet _serviceTrajet = new ServiceTrajet(con);
+
+    public void ajout(Trajet trajet) {
+        _serviceTrajet.ajout(trajet);
+    }
+
+    public void afficher() {
+        _serviceTrajet.afficher();
+    }
+
+    public void modifier(Trajet trajet) {
+        _serviceTrajet.modifier(trajet);
+    }
+
+    public void supprimer(Trajet trajet) {
+        _serviceTrajet.supprimer(trajet);
+    }
+
+    public int getSize() {
+        return _serviceTrajet.getSize();
+    }
+
+    public void truncate() {
+        _serviceTrajet.truncate();
+    }
+}
---
+++ src/main/java/org/example/teleporti/Services/IServiceTrajet.java
@@ -0,0 +1,14 @@
+package org.example.teleporti.Services;
+
+
+import org.example.teleporti.Entities.Trajet;
+
+public interface IServiceTrajet {
+    public void ajout(Trajet newTrajet);
+
+    public void afficher();
+
+    public void modifier(Trajet trajet);
+
+    public void supprimer(Trajet trajet);
+}
