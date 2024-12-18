bcd236dfbf44fa06afa378c3204376dca15496a6
itschaima <chaimaabbassi0102@gmail.com>
2024-12-18 16:22:55 +0100

    iserviceuser adapted

--- src/main/java/org/example/teleporti/Services/IServiceUser.java
+++ src/main/java/org/example/teleporti/Services/IServiceUser.java
@@ -3,11 +3,15 @@ package org.example.teleporti.Services;
 import org.example.teleporti.Entities.User;
 
 public interface IServiceUser {
+
     public void ajout(User newUser);
+
 
     public void afficher();
 
+
     public void modifier(User user);
+
 
     public void supprimer(User user);
 }
