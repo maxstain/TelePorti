98a62a04b1155b179d45151f59f3b0f20c0f03fb
maxstain <firaschabchoub@hotmail.com>
2025-01-06 14:43:36 +0100

    Working on the UI/UX.

--- src/main/java/org/example/teleporti/SceneControllers/MapsViewController.java
+++ src/main/java/org/example/teleporti/SceneControllers/MapsViewController.java
@@ -2,15 +2,20 @@ package org.example.teleporti.SceneControllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.example.teleporti.Controllers.AuthController;
 import org.example.teleporti.Entities.User;
+import org.example.teleporti.Utils.Router;
 
 public class MapsViewController {
+
+    private final AuthController authController = new AuthController();
 
     @FXML
     private Label welcome = new Label("");
 
     protected User currentUser;
 
+    @FXML
     public void initialize() {
     }
 
@@ -21,5 +26,63 @@ public class MapsViewController {
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
         setWelcomeMessage(currentUser.getPrenom());
+    }
+
+    @FXML
+    public void onLogout() {
+        try {
+            authController.logout(currentUser.getId());
+            Router.goToLogin(welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToStats() {
+        try {
+            Router.goToStats(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToDashboard() {
+        try {
+            Router.goToDashboard(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToProfile() {
+        try {
+            Router.goToProfile(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToMaps() {
+        try {
+            Router.goToMaps(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToUsers() {
+        try {
+            Router.goToUsers(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToSettings() {
+        try {
+            // Router.goToSettings(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/teleporti/SceneControllers/StatsViewController.java
+++ src/main/java/org/example/teleporti/SceneControllers/StatsViewController.java
@@ -1,6 +1,5 @@
 package org.example.teleporti.SceneControllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import org.example.teleporti.Controllers.AuthController;
@@ -30,7 +29,24 @@ public class StatsViewController {
     }
 
     @FXML
-    public void onGoToHome() {
+    public void onLogout() {
+        try {
+            authController.logout(currentUser.getId());
+            Router.goToLogin(welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToStats() {
+        try {
+            Router.goToStats(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToDashboard() {
         try {
             Router.goToDashboard(currentUser, welcome);
         } catch (Exception e) {
@@ -38,11 +54,33 @@ public class StatsViewController {
         }
     }
 
-    @FXML
-    public void onLogout() {
+    public void onGoToProfile() {
         try {
-            authController.logout(currentUser.getId());
-            Router.goToLogin(welcome);
+            Router.goToProfile(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToMaps() {
+        try {
+            Router.goToMaps(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToUsers() {
+        try {
+            Router.goToUsers(currentUser, welcome);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGoToSettings() {
+        try {
+            // Router.goToSettings(currentUser, welcome);
         } catch (Exception e) {
             e.printStackTrace();
         }
