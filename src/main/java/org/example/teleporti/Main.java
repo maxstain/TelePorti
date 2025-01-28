cd015b909d262921e3df9684b39fca9e2d9164a7
maxstain <firaschabchoub@hotmail.com>
2025-01-28 12:08:49 +0100

    Working on the UI/UX.

--- src/main/java/org/example/teleporti/Main.java
+++ src/main/java/org/example/teleporti/Main.java
@@ -18,40 +18,41 @@ public class Main {
         _userController.createUserTableInDatabase();
 
         User user1 = new User(_userController.getSize() + 1, "Abbassi", "Chaima", 23, "chaimaabbassi@gmail.com", "chaima123", "Chauffeur", "Tunis", "Tunis", "Tunis", "12345678");
+        _userController.ajout(user1);
+
         User user2 = new User(_userController.getSize() + 1, "Chabchoub", "Firas", 25, "firaschabchoub@gmail.com", "Firas123", "Admin", "Sfax", "Sfax", "Sfax", "12345678");
+        _userController.ajout(user2);
 
         User user3 = new User(_userController.getSize() + 1, "Ben Rhouma", "Rached", 24, "rachedbrhouma@gmail.com", "rached123", "Client", "Tunis", "Tunis", "Tunis", "12345678");
+        _userController.ajout(user3);
 
         User user4 = new User(_userController.getSize() + 1, "Belhaj Amor", "Ahmed", 25, "ahmedbamor@gmail.com", "ahmed123", "Client", "Tunis", "Tunis", "Tunis", "12345678");
+        _userController.ajout(user4);
 
         Trajet trajet1 = new Trajet(_trajetController.getSize() + 1, user1.getId(), "Tunis", "Sousse", 3, 20, 50.0f);
+        _trajetController.ajout(trajet1);
 
         Trajet trajet2 = new Trajet(_trajetController.getSize() + 1, user1.getId(), "Tunis", "Sfax", 4, 30, 70.0f);
+        _trajetController.ajout(trajet2);
 
         Trajet trajet3 = new Trajet(_trajetController.getSize() + 1, user1.getId(), "Tunis", "Bizerte", 1, 10, 30.0f);
+        _trajetController.ajout(trajet3);
 
         Reservation reservation1 = new Reservation(_reservationController.getSize() + 1, user3.getId(), trajet1.getId(), "En cours");
+        _reservationController.ajout(reservation1);
 
         Reservation reservation2 = new Reservation(_reservationController.getSize() + 1, user4.getId(), trajet1.getId(), "En cours");
+        _reservationController.ajout(reservation2);
 
         Reservation reservation3 = new Reservation(_reservationController.getSize() + 1, user3.getId(), trajet2.getId(), "En cours");
+        _reservationController.ajout(reservation3);
 
         // _userController.truncate();
-        _userController.ajout(user1);
-        _userController.ajout(user2);
-        _userController.ajout(user3);
-        _userController.ajout(user4);
-//        _userController.afficher();
-        _trajetController.truncate();
-        _trajetController.ajout(trajet1);
-        _trajetController.ajout(trajet2);
-        _trajetController.ajout(trajet3);
-        _trajetController.afficher();
-        _reservationController.truncate();
-        _reservationController.ajout(reservation1);
-        _reservationController.ajout(reservation2);
-        _reservationController.ajout(reservation3);
-        _reservationController.afficher();
+        // _userController.afficher();
+        // _trajetController.truncate();
+        // _trajetController.afficher();
+        // _reservationController.truncate();
+        // _reservationController.afficher();
 
     }
 
