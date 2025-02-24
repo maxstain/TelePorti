7795dc6a6069e64fd825309f571a50889861c659
maxstain <firaschabchoub@hotmail.com>
2025-02-24 17:59:33 +0100

    Working on the Add Modal functionality.

--- src/main/java/org/example/teleporti/SceneControllers/LoginViewController.java
+++ src/main/java/org/example/teleporti/SceneControllers/LoginViewController.java
@@ -1,21 +1,19 @@
 package org.example.teleporti.SceneControllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import org.example.teleporti.Controllers.AuthController;
 import org.example.teleporti.Entities.User;
 import org.example.teleporti.Utils.Router;
-import org.kordamp.bootstrapfx.BootstrapFX;
-
-import java.io.IOException;
 
 public class LoginViewController {
+
+    protected final AuthController authController = new AuthController();
+
     @FXML
     protected Label errorText;
     @FXML
@@ -25,7 +23,6 @@ public class LoginViewController {
     @FXML
     protected CheckBox staySignedInCheckBox;
 
-    protected final AuthController authController = new AuthController();
 
     @FXML
     protected void onFormSubmit() {
